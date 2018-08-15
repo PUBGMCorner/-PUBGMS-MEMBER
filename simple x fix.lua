197bdb01073617706f4be0a586954d332555c898
PUBG Mobile Corner <42271883+PUBGMCorner@users.noreply.github.com>
2018-08-16 05:01:22 +0700

    simple FIXED

---
+++ simple x fix.lua
@@ -0,0 +1,519 @@
+gg.toast(os.date([[
+Today: %Y/%m/%d Time: %H:%M:%S
+www.youtube.com/c/PUBGMCorner]]))
+gg.alert("⭐PUBGMS Simple Script 0.8.4 VVIP⭐")
+
+gg.alert("⭐VVIP ID CHECK⭐️")
+pass = "XXX"
+prompt = gg.prompt({
+  "\240\159\148\142ENTER VVIP ID"
+}, {
+  [1] = 0
+}, {
+  [1] = "number"
+})
+function right()
+  gg.alert("\226\156\148 VERIFIED!")
+end
+function wrong()
+  gg.alert("\226\156\150 REJECTED")
+  os.exit()
+end
+if prompt[1] == pass then
+  right()
+else
+  wrong()
+end
+HOME = 1
+
+gg.toast("VERIFIED!")
+
+
+gg.alert("⭐DEVICE VERIVIED VVIP MEMBER⭐")
+
+gg.alert("🌟️ WELCOME KEPALA SUKU BARBAR 🌟️")
+
+HOME = 1
+function HOME()
+    HM = gg.choice({
+    "⭕ [LOBBY] HEADSHOT 1 CLICK ⭕",
+    "⭕️ [LOBBY] BARBAR 1 CLICK ⭕",
+    "⏺️ MENU BARBAR ⏺️",
+	"⭕ ONE CLICK LANDED ⭕",
+	"⭕ ONE CLICK JEEP ⭕",
+	"🌟️ Info & Update Log 🌟️",
+    "❎ EXIT SCRIPT ❎",
+    "☎️ CONTACT US ☎️"
+  }, nil, [[
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+  ⭐⭐⭐⭐SPECIAL SCRIPT⭐⭐⭐⭐
+      ⭐️KEPALA SUKU BARBARIAN⭐️
+⭐️MODED BY : @KopralDjonoPUBGMC⭐️
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖]])
+  if HM == nil then
+  else
+  if HM == 1 then
+    LHOC()
+  end
+  if HM == 2 then
+    LBOC()
+  end
+  if HM == 3 then
+    MBAR()
+  end
+  if HM == 4 then
+    OCL()
+  end
+  if HM == 5 then
+    OCJ()
+  end  
+  if HM == 6 then
+    INFO()
+  end
+  if HM == 7 then
+    EXIT()
+  end
+    if HM == 7 then
+    MC()
+  end
+ end
+ end
+
+function LHOC()
+  gg.clearResults()
+  gg.searchNumber("1868784978;1850305641;28518", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1868784978", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("1868756421", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("100000", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("No Recoil All Gun Activated")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1.4012985e-45;1;1;1;1;100000::100", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("0.5", gg.TYPE_FLOAT)
+
+
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("-88.66608428955;26:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-460", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.searchNumber("-88.73961639404;28:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-560", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("500", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("NEO HEAD AIMBOT ACTIVATED")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("3.5F;1F;1F;1F;200F;20F:512", gg.TYPE_DWORD, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("200", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(850)
+  gg.editAll("9999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("3.5F;1F;1F;1F;9999F;20F:512", gg.TYPE_DWORD, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("3.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(850)
+  gg.editAll("-9999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Average Aimbot Activated")
+  gg.toast("PUBG Mobile Script")
+end
+
+function LBOC()
+  gg.clearResults()
+  gg.searchNumber("1868784978;1850305641;28518", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1868784978", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("1868756421", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1750294898;1415932769;1819307365", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("100000", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("No Recoil All Gun Activated")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1.4012985e-45;1;1;1;1;100000::100", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("0.5", gg.TYPE_FLOAT)
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1F;-8.6457681e12F;15F;28F;16F;26F;8F;18F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("15.0F;28.0F;16.0F;26.0F;8.0F;18.0F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(500)
+  gg.editAll("75", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Magic Bullet Activated")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("3.5F;1F;1F;1F;200F;20F:512", gg.TYPE_DWORD, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("200", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(850)
+  gg.editAll("9999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("3.5F;1F;1F;1F;9999F;20F:512", gg.TYPE_DWORD, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("3.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(850)
+  gg.editAll("-9999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Average Aimbot Activated")
+  gg.toast("PUBG Mobile Script")
+end
+
+function OCL()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("4.814603e21;3.5032462e-44;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("-5.5693206e-40;4.814603e21;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("5.1848043e-44;-1.0285578e-38;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("304.00009155273;3.7615819e-37;2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(30)
+  gg.editAll("120", gg.TYPE_FLOAT)
+  gg.toast("WallHack Global Activated")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("32,769;-2,134,900,722", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("32769", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("32777", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("Body Green Activated")
+ 
+  gg.clearResults()
+  gg.setRanges(gg.REGION_BAD)
+  gg.clearResults()
+  gg.searchNumber("100F;1F;1,008,981,770D:99", gg.TYPE_FLOAT, false, gg.SING_EQUAL, 0, -1)
+  gg.searchNumber("100", gg.TYPE_FLOAT, false, gg.SING_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("-99", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.toast("Dark Sky Activated")
+
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.searchNumber("8.0F;1.20000004768F;0.80000001192F;1.5F;0.80000001192F;1.5F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("8", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("0", gg.TYPE_FLOAT)
+  gg.toast("No Grass Activated")
+end
+
+function OCJ()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("150;85;45;-129;-85", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber(45, gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("99996", gg.TYPE_FLOAT)
+  gg.clearResults(1314520)
+  gg.toast("Submarine Jeep Activated")
+  gg.toast("PUBG Mobile Script")
+
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("0.647058857", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("-999", gg.TYPE_FLOAT)
+  gg.toast("Max Speed Jeep Activated")
+  gg.toast("PUBG Mobile Script")
+end
+
+function MBAR()
+    MBB = gg.choice({
+    "⭕ FR AKM",
+    "⭕️ FR M416",
+    "⭕️ FR SCAR-L",
+	"⏺️ WALLSHOT ⏺️",
+	"⏺️ SIT SCOPE ⏺️",
+	"⏺️ OFF SIT SCOPE ⏺️",
+	"⏺️ SUPER SPEED ⏺️",
+	"⏺️ HIGH JUMP ⏺️",
+	"⏺️ LONG JUMP ⏺️",
+	"⏺️ FAST SWITCH ⏺️",
+    "⏺️ MAGIC BULLET  ⏺️",
+	"⏺️ ULTRA AIMBOT ⏺️",
+    "⏺️ HEADSHOT NEW ⏺️",
+	"⏪⏪⏪ BACK ⏪⏪⏪",
+  }, nil, [[
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+  ⭐⭐KOPRAL⭐DJONO⭐PUBGMC⭐⭐
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖]])
+if MBB == nil then
+  else
+  if MBB == 1 then
+    AKM()
+  end
+  if MBB == 2 then
+    M416()
+  end
+  if MBB == 3 then
+    SCAR()
+  end
+  if MBB == 4 then
+    WALL()
+  end
+  if MBB == 5 then
+    STSC()
+  end  
+    if MBB == 6 then
+    OFF()
+	end
+  if MBB == 6 then
+    SPEED()
+  end
+  if MBB == 7 then
+    HJUMP()
+  end
+  if MBB == 8 then
+    LJUMP()
+  end
+  if MBB == 9 then
+    FSWITCH()
+  end
+  if MBB == 10 then
+    MAGIC()
+  end
+  if MBB == 11 then
+    ULTRA()
+  end
+  if MBB == 12 then
+    HEAD()
+  end  
+  if MBB == 13 then
+    HOME()
+  end
+  end
+  end
+function AKM()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("30D;10D;0F~1F;257D;3D::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.10000000149", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(50)
+gg.editAll("0.001", gg.TYPE_FLOAT)
+gg.toast('Speed AKM Activated')
+end
+function M416()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("30D;10D;0F~1F;257D;3D::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.08600000292", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(50)
+gg.editAll("0.03200000272", gg.TYPE_FLOAT)
+gg.toast('Speed M416 Activated')
+end
+function SCAR()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("30D;10D;0F~1F;257D;3D::17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.09600000083", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(50)
+gg.editAll("0.02800000022", gg.TYPE_FLOAT)
+gg.toast('Speed SCAR-L Activated')
+end
+function WALL()
+  gg.setRanges(gg.REGION_C_BSS)
+  gg.clearResults()
+  gg.searchNumber("869,711,765D;2;1::55", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("0", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Neo WallShot Activated")
+  gg.toast("PUBG Mobile Script")
+end
+function STSC()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,092,081,726;1,003,658,240;923,795,456", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,092,081,726", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("1,135,081,726", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("Sit On Scope Activated")
+end
+function OFF()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1,135,081,726;1,003,658,240;923,795,456", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1,135,081,726", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("1,092,081,726", gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.toast("Sit On Scope Turned Off")
+end
+function SPEED()
+  gg.clearResults()
+  gg.searchNumber('999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber('10.90734863281;0.00999999978', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber('10.9073486328', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(300)
+  gg.editAll('10.211', gg.TYPE_FLOAT)
+  gg.toast('Speed Hack Activated')
+end
+function HJUMP()
+  gg.clearResults(850)
+  gg.searchNumber("1;35;443;0.5;55;0.57357645035", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(850)
+  print("Replaced: ", gg.editAll("0.3", gg.TYPE_FLOAT))
+  gg.clearResults(850)
+  gg.toast("High Jump Active")
+end
+function LJUMP()
+  gg.clearResults()
+  gg.searchNumber("-980.0F;0.30000001192F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("-980", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("-550", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Long Jump telah aktif")
+end
+function FSWITCH()
+  gg.clearResults()
+  gg.searchNumber('0.83333331347;1;0.83333331347::321', gg.TYPE_FLOAT,false,gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber('0.83333331347', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll('0.000001',gg.TYPE_FLOAT) 
+  gg.toast('Fast switch Weapon Activated')
+end
+function MAGIC()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1F;-8.6457681e12F;15F;28F;16F;26F;8F;18F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("15.0F;28.0F;16.0F;26.0F;8.0F;18.0F:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(500)
+  gg.editAll("75", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Magic Bullet Activated")
+end
+function ULTRA()
+  gg.searchNumber("999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.clearResults()
+  gg.searchNumber("3.5;1;200;20::999", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("3.5;1;200;20", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(300)
+  gg.editAll("-1.0e10", gg.TYPE_FLOAT)
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.clearResults()
+  gg.searchNumber("8;16;18", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("8;16;18", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1000)
+  gg.editAll("38.5", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Maximum Aimbot Activated")
+  gg.toast("PUBG Mobile Script")
+end
+function HEAD()
+  gg.setRanges(gg.REGION_BAD)
+  gg.searchNumber("-88.66608428955;26:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-460", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.searchNumber("-88.73961639404;28:512", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("-560", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(10)
+  gg.editAll("500", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("NEO HEAD AIMBOT ACTIVATED")
+end
+function MC()
+  KR = gg.choice({
+    "⭐ Telegram  : @PUBGMSCRIPT",
+    "⭐ Instagram : @PubgMobileScript",
+	"⭐ Whatsapp  : +62 823 3213 9274",
+	"⭐ Channel   : youtube.com/PUBGMCorner",
+    "⏪⏪⏪"
+  }, nil, "☎️ CONTACT US FOR VIP AND VVIP SCRIPT ☎️")
+  if KR == 1 then
+    MC()
+  end
+  if KR == 2 then
+    MC()
+  end
+    if KR == 3 then
+    MC()
+  end
+  if KR == 4 then
+    MC()
+  end
+  if KR == 5 then
+    HOME()
+  end
+  HOMEDM = -1
+end
+function INFO()
+gg.alert("⛔ WARNING : Don't Combine Any Headshot Hacks with Any Magic Bullet Hacks")
+gg.alert("💯 Fixed : Headshot - Jump - Speed Hack - Magic Bullet Pshycho")
+end  
+function EXIT()
+  print(os.date("PUBG MOBILE VVIP SCRIPT"))
+  print("☑️ Channel  : youtube.com/PUBGMCorner")
+  print("☑️ Telegram : @PUBGMSCRIPT")
+  print("☑️ Instagram: @PubgMobileScript")
+  print("☑️ Whatsapp : +62 823 3213 9274")
+  print(" ☎️ CONTACT US FOR DETAIL OF OUR HACKS ☎️")
+  os.exit()
+end
+while true do
+  if gg.isVisible(true) then
+    HOMEDM = 1
+    gg.setVisible(false)
+  end
+  if HOMEDM == 1 then
+    HOME()
+  end
+end
